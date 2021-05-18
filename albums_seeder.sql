661fdb64662360cb49de77e358bd4381243ded46
John Pierce <pierce.johndaniel@gmail.com>
2021-05-18 18:23:56 -0500

    feat: added albums_seeder.sql file

---
+++ albums_seeder.sql
@@ -0,0 +1,6 @@
+USE codeup_test_db;
+
+INSERT INTO albums(artist_name,  record_name, release_date, sales ,genre)
+    VALUE ('Michael Jackson', 'Thriller', '1982', '66', 'Pop, post-disco, funk, rock'),
+--     VALUE ('AC/DC', 'Back in Black', '1980', 'Hard rock', '50'),
+--     VALUE ('Pink Floyd', 'The Dark Side of the Moon', '1973', 'Progressive rock' '45');
