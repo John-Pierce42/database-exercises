5ded601b8687faf76570e1d686e59eb575da2050
John Pierce <pierce.johndaniel@gmail.com>
2021-05-21 10:06:52 -0500

    feat: finished where_exercises

--- where_exercises.sql
+++ where_exercises.sql
@@ -15,6 +15,22 @@ WHERE last_name LIKE '%q%';
 
 # part two.
 
+SELECT 'Names who is also male ' AS 'Employees';
 SELECT  first_name, last_name
 FROM employees
-WHERE last_name OR 'Irena' OR 'Vidya' OR 'Lipner' OR 'Maya';
+WHERE last_name OR 'Irena' OR 'Vidya' OR 'Lipner' OR 'Maya'
+AND gender = 'M';
+
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%E';
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%E' AND 'E%';
+
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%q%' AND last_name != '%qu%';
