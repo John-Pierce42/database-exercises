4032a73bf3ecad93d8983c0993201dddff63980f
John Pierce <pierce.johndaniel@gmail.com>
2021-05-21 10:27:09 -0500

    feat: working on order by

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,13 +1,25 @@
 USE employees;
 
-SELECT emp_no, first_name, last_name
+SELECT first_name
 FROM employees
-ORDER BY first_name IN ('Irena', 'Vidya', 'Lipner', 'Maya') ASC;
+WHERE first_name IN ('Irena', 'Vidya', 'Lipner', 'Maya')
+ORDER BY first_name ASC;
 
-SELECT last_name
+SELECT first_name, last_name
 FROM employees
-WHERE last_name LIKE 'E%';
+WHERE first_name IN ('Irena', 'Vidya', 'Lipner', 'Maya')
+ORDER BY first_name ASC, last_name ASC;
 
-SELECT last_name
+
+SELECT first_name, last_name
 FROM employees
-WHERE last_name LIKE '%q%';
+WHERE first_name IN ('Irena', 'Vidya', 'Lipner', 'Maya')
+ORDER BY last_name ASC, first_name ASC;
+
+-- SELECT last_name
+-- FROM employees
+-- WHERE last_name LIKE 'E%';
+--
+-- SELECT last_name
+-- FROM employees
+-- WHERE last_name LIKE '%q%';
