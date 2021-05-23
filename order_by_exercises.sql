79b0cb19b64bd7b8dc947849d20d3253b5051c89
John Pierce <pierce.johndaniel@gmail.com>
2021-05-23 12:54:12 -0500

    feat: added group_by exercise and finishing up order_by

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -16,10 +16,11 @@ FROM employees
 WHERE first_name IN ('Irena', 'Vidya', 'Lipner', 'Maya')
 ORDER BY last_name ASC, first_name ASC;
 
--- SELECT last_name
--- FROM employees
--- WHERE last_name LIKE 'E%';
---
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+ORDER BY emp_no;
+
 -- SELECT last_name
 -- FROM employees
 -- WHERE last_name LIKE '%q%';
